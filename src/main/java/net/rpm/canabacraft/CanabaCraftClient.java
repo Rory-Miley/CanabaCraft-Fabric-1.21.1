e8475038204cc4091253a8d93d5575aa2d92c52b
Rory Miley <mileyr@tcd.ie>
2024-09-30 16:53:15 -0400

    Initial commit

---
+++ src/main/java/net/rpm/canabacraft/CanabaCraftClient.java
@@ -0,0 +1,10 @@
+package net.rpm.canabacraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CanabaCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/rpm/canabacraft/CanabaCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rpm.canabacraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CanabaCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
